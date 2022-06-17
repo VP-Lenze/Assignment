b220c4aae1215466c473e257fda30af74ac09bf1
VP-Lenze <Vedant.Padalkar@lenze.com>
2022-06-17 10:46:55 +0530

    Add project files.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n-----------------Assignment 2-----------------\n\n");
+
+            string outputFile = @"Assignment 2_output.txt";
+
+            try
+            {
+                Console.Write("Enter size of array : ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                if (size >= 1)
+                {
+                    string[] stringArray = new string[size];
+                    for (int i = 0; i < size; i++)
+                    {
+                        stringArray[i] = Console.ReadLine();
+                    }
+                    File.WriteAllLines(outputFile, stringArray);
+                    string[] fileLines = File.ReadAllLines(outputFile);
+                    int pairs = 0;
+                    Console.WriteLine($"Input array : {string.Join(",", fileLines)}");
+                    Console.Write("Input number to find pairs : ");
+                    int key = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < fileLines.Length; i++)
+                    {
+                        if (fileLines[i] == key.ToString())
+                        {
+                            pairs++;
+                        }
+                    }
+                    Console.WriteLine($"Total pairs : {Math.Floor((double) pairs / 2)}\n");
+                }
+                else
+                {
+                    Console.WriteLine("Minimum length of array should be 30\n");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n-----------------Assignment 1-----------------\n\n");
+
+            string file = @"Assignment 1.txt";
+            if (File.Exists(file))
+            {
+                String[] lines = File.ReadAllLines(file);
+                Console.Write("Enter word to search : ");
+                String key = Console.ReadLine();
+                int totalOccurrance = 0, index = 0;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    MatchCollection mc = Regex.Matches(lines[i], String.Format(@"\b(?i){0}\b", key));
+                    int length = mc.Count;
+                    if (length > 0)
+                    {
+                        totalOccurrance += length;
+                    }
+                    if (index == 0 && totalOccurrance > 1)
+                    {
+                        index = i + 1;
+                    }
+                }
+                Console.WriteLine($"Total Occurrance : {totalOccurrance}\n");
+                if (index > 0)
+                {
+                    Console.WriteLine($"Answer : Line {index}\n");
+                }
+            }
+        }
+    }
+}
